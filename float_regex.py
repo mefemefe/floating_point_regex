5e541e6a20358f4a129206450bbe1c873b0b262e
mefemefe <50670336+mefemefe@users.noreply.github.com>
2021-07-10 09:58:29 -0300

    Update float_regex.py

--- float_regex.py
+++ float_regex.py
@@ -1,6 +1,6 @@
 math = 'f4.0' # any string
 
-# determine if string contains a floating point number
+# determine if string is a floating point number (in this example, the string CONTAINS a float but it's not a float since it has an alphabetic character)
 import re
 float_check = re.search('^\-*\+*[0-9]*\.[0-9]+$', math)
 
